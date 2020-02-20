66eabf64f914b1f2aa6c16f93138cb0872a7c0fc
megastar1212 <31452789+megastar1212@users.noreply.github.com>
2020-02-20 16:12:46 -0500

    Update BsonSchema.js

--- Class/BsonSchema.js
+++ Class/BsonSchema.js
@@ -13,7 +13,7 @@ const valid_schemas = [
 class Schema {
 
   constructor(schema) {
-    if(!isObject(schema)) throw new BsonError("Schema", "El parametro del constructor Schema debe ser un 'Object'")
+    if(!schema || !isObject(schema)) throw new BsonError("Schema", "El parametro del constructor Schema debe ser un 'Object'")
     if(!check_schema(schema, valid_schemas)) throw new BsonError("Schema", "El formato del Schema es incorrecto.")
 
     this.schema = schema
